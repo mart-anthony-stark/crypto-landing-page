cc6218c8fbf1e5f641bbc877024e92ea882edee2
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-22 21:23:28 +0800

    add coin card component

---
+++ src/components/CoinCard.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="cont">
+    <div class="head">
+      <img :src="props.img" class="coin-img" />
+
+      <div class="name">
+        <span class="coin">{{ props.slug.toUpperCase() }}</span>
+        <span>{{ props.name.toUpperCase() }}</span>
+      </div>
+
+      <img src="../assets/coins/see-more-ic.png" alt="" />
+    </div>
+
+    <div class="bottom">
+      <div class="stats">
+        <h4>{{ props.value }}</h4>
+        <span>{{ props.rate }}</span>
+      </div>
+
+      <img src="../assets/coins/chart-state1.png" alt="" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  name: String,
+  slug: String,
+  img: String,
+  value: String,
+  rate: String,
+});
+</script>
+
+<style scoped>
+.cont {
+  width: 250px;
+  height: 150px;
+  background: rgba(129, 129, 129, 0.1);
+  backdrop-filter: blur(1px);
+  border-radius: 25px;
+  border: 1px solid rgba(126, 126, 126, 0.4);
+  padding: 0.3rem 1rem;
+  font-family: "Roboto", sans-serif;
+}
+
+.head {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid rgba(131, 131, 131, 0.473);
+  padding-bottom: 0.5rem;
+  margin-bottom: 0.5rem;
+}
+.coin-img {
+  height: 30px;
+  width: 30px;
+}
+
+.name {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+.name span:nth-child(1) {
+  font-weight: 900;
+}
+.name span:nth-child(2) {
+  background: rgb(204, 204, 204);
+  border-radius: 3px;
+  color: black;
+  font-size: 8px;
+  padding: 5px;
+}
+
+.bottom {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.bottom .stats {
+  line-height: 2rem;
+}
+.bottom .stats span {
+  font-size: 14px;
+  color: gray;
+}
+</style>
