ac41f78f36902c0d45733b803e0fde934f12aa3f
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-22 21:23:18 +0800

    add button component

---
+++ src/components/ButtonComp.vue
@@ -0,0 +1,25 @@
+<template>
+  <button>{{ props.text }}</button>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  text: String,
+});
+</script>
+
+
+<style scoped>
+@import "../css/variables.css";
+
+button {
+  padding: 10px 20px;
+  border: none;
+  background: var(--c-accent);
+  border-radius: 5px;
+  color: aliceblue;
+  cursor: pointer;
+}
+</style>
