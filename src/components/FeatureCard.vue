ab7e6b111eb336e1b2097f9e7ee32ad955ea1946
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-23 20:47:40 +0800

    add styles

--- src/components/FeatureCard.vue
+++ src/components/FeatureCard.vue
@@ -1,3 +1,19 @@
 <template>
-  
+  <div class="card"></div>
 </template>
+
+<style scoped>
+.card {
+  width: 296px;
+  height: 343px;
+
+  /* High Fidelity Color/Card Background */
+
+  background: rgba(255, 255, 255, 0.02);
+  border: 1px solid rgba(255, 255, 255, 0.05);
+  backdrop-filter: blur(200px);
+  /* Note: backdrop-filter has minimal browser support */
+
+  border-radius: 18px;
+}
+</style>
