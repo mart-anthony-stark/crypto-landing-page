0960590a6affd8e277198d23f03da239ed96fea0
mart-anthony-stark <martsalazar0@gmail.com>
2022-08-23 20:38:09 +0800

    add bg

--- src/components/sections/FeaturesSection.vue
+++ src/components/sections/FeaturesSection.vue
@@ -1,3 +1,22 @@
 <template>
   <section></section>
 </template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style scoped>
+section {
+  background: rgb(50, 1, 40);
+  background: radial-gradient(
+    circle,
+    rgba(24, 3, 43, 1) 46%,
+    rgba(19, 1, 34, 1) 100%
+  );
+}
+</style>
