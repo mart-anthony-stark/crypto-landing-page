f966274a41a79d68556da58fd727826890cfb059
mart-anthony-stark <martsalazar0@gmail.com>
2022-09-18 20:25:15 +0800

    add content and fixed styles

--- src/components/FeatureCard.vue
+++ src/components/FeatureCard.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="card">
-    <img src="../assets/svg/features/manage.svg" alt="" />
-    
+    <img class="icon" src="../assets/svg/features/manage.svg" alt="feature" />
+    <div class="content">
+      <h3>Manage Portfolio</h3>
+      <p>
+        Buy and sell popular digital currencies, keep track of them in the one
+        place.
+      </p>
+    </div>
+    <a class="see"
+      >See Explained <img src="../assets/svg/features/arrow.svg" alt="see more"
+    /></a>
   </div>
 </template>
 
 <style scoped>
+@import "../css/variables.css";
+
 .card {
   width: 296px;
   height: 343px;
@@ -18,5 +29,27 @@
   /* Note: backdrop-filter has minimal browser support */
 
   border-radius: 18px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  padding: 2rem;
+}
+
+.icon {
+  height: 80px;
+}
+
+.content h3 {
+  margin-bottom: 1rem;
+}
+
+.see {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  color: var(--c-accent);
+  cursor: pointer;
 }
 </style>
